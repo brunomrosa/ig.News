bb315fbbf25435fbb4b22275bbbf9121235e1747
brunomrosa <brunomoraes52569@gmail.com>
2022-05-04 17:51:56 -0300

    stripe integration with faunadb

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,40 @@
+import { api } from "../../services/api";
+import { signIn, useSession } from "next-auth/react";
 import styles from "./styles.module.scss";
+import { getStripeJs } from "../../services/stripe-js";
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
 export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
+  const { status } = useSession();
+
+  const handleSubscribe = async () => {
+    if (status === "unauthenticated") {
+      signIn("github");
+      return;
+    }
+
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe
     </button>
   );

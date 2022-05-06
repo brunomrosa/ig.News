836ca27cbbfb6e5e7dd86ac456103d098cbbd9e7
brunomrosa <brunomoraes52569@gmail.com>
2022-05-06 18:29:19 -0300

    prismic + posts page

---
+++ .slicemachine/assets/slices/MySlice/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/MySlice';
+
+export default {
+  title: 'slices/MySlice'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"my_slice","items":[],"primary":{"title":[{"type":"heading1","text":"Enable B2C channels","spans":[]}],"description":[{"type":"paragraph","text":"Ad eu consectetur ullamco excepteur est irure aliqua adipisicing. Culpa eu esse officia excepteur consectetur. Enim ullamco id officia tempor in consectetur eu Lorem dolore irure Lorem id.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
